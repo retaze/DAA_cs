64bc8188b0f8a74b4b8f8bac77577a0c10860f10
Lars Gruber <lars.gruber@outlook.com>
2018-08-13 11:28:05 +0200

    DAA-Changes

---
+++ Test1/Test1/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test1
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            CPerson person = new CPerson();
+            person.SetName();
+            person.SchreibeName();
+
+        }
+    }
+
+    class CPerson
+    {
+        private string name;
+
+        public void SetName()
+        {
+            name = "Kaiser";
+        }
+
+        public void SchreibeName()
+        {
+            Console.WriteLine(name);
+            Console.WriteLine();
+        }
+    }
+
+}
+
--- Test1/Test1/Program.cs
+++ Test1/Test1/Program.cs
@@ -1,18 +1,45 @@
-﻿using System;
+﻿/*using System;
 
 namespace Test1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void test(string[] args)
         {
             int eingabe;
-            Console.WriteLine("Eingabe:");
+            Console.WriteLine("Zahl eingeben:");
             eingabe = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(eingabe);
 
+            Console.WriteLine("Zahl eingeben:");
+            eingabe = Convert.ToInt32(Console.ReadLine());
+
+            if
+                (eingabe == 9)
+            {
+                Console.WriteLine("Zahl eingeben:");
+                eingabe = Convert.ToInt32(Console.ReadLine());
+            }
+            else {
+
+                switch (eingabe)
+                {
+                    case 1:
+                        Console.WriteLine("Sie haben eine 1 eingegeben");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Sie haben eine 2 eingegeben");
+                        break;
+
+                    default:
+                        Console.WriteLine("Sie haben weder 1 noch 2 eingegeben");
+                        break;
+
+                }
+            }
+            
         }
 
 
     }
-}
+}*/
