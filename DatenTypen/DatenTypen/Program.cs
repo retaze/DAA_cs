5c75b618933b39f2189053c536f4c275a67a1fb3
Lars Gruber <lars.gruber@outlook.com>
2018-08-21 12:08:19 +0200

    DAA

---
+++ DatenTypen/DatenTypen/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DatenTypen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wert1 = 25;
+
+            int wert2;
+            wert2 = 15;
+
+            int summe = wert1 + wert2;
+
+            int wert3, wert4, wert5;
+
+            double d1 = 3.5;
+            double d2 = 1.337;
+            double ergebnis = d1 / d2;
+
+            Console.WriteLine("Ergebniss ist "+ergebnis);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Hallo Welt!");
+            Console.WriteLine("Hallo Lars");
+
+            Console.ReadKey();
+        }
+    }
+}
