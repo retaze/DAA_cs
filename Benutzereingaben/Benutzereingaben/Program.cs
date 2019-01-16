64497ffc16d84b16788fe33dab061926f7be0ca1
Lars Gruber <lars.gruber@outlook.com>
2019-01-16 11:45:38 +0100

    DAA changes

---
+++ Benutzereingaben/Benutzereingaben/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Benutzereingaben
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculate());
+            Console.ReadKey();
+            
+        }
+
+        public static int Calculate()
+        {
+            Console.WriteLine("Bitte trage die erste Zahl ein.");
+            string num1Input = Console.ReadLine();
+            Console.WriteLine("Bitte trage die zweite Zahl ein.");
+            string num2Input = Console.ReadLine();
+
+            int num1 = int.Parse(num1Input);
+            int num2 = int.Parse(num2Input);
+
+            int result = num1 + num2;
+            return result;
+
+        }
+    }
+
+}
