469db53adbf574a877ce545c007a685f8477c7be
Lars Gruber <lars.gruber@outlook.com>
2019-01-18 08:52:11 +0100

    DAA changes

--- TryCatchUndFinally/TryCatchUndFinally/Program.cs
+++ TryCatchUndFinally/TryCatchUndFinally/Program.cs
@@ -8,9 +8,46 @@ namespace TryCatchUndFinally
         {
             Console.WriteLine("Bitte trage eine Zahl ein");
             string userInput = Console.ReadLine();
+            int ergebnis;
 
-            int userInputAsInt = int.Parse(userInput);
+            try
+            {
+                int userInputAsInt2 = int.Parse(userInput);
+                ergebnis = userInputAsInt2 / 0;
+                Console.WriteLine(ergebnis);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Durch 0 teilen ist nicht erlaubt");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Grundlegender Fehler!");
+            }
+            /*
+            try
+            {
+                int userInputAsInt = int.Parse(userInput);
+            }
+            catch (FormatException)
+            {
 
+                Console.WriteLine("Format war inkorrekt. Du hättest eine Nummer eintragen sollen!");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("ArgumentNullExeption, der Wert war leer (null).");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("OverflowException, der eingegebene Wert war zu lang.");            
+            }
+            finally
+            {
+                Console.WriteLine("Ich werde sowieso ausgeführt");
+            }
+            */
+            Console.ReadKey();
         }
     }
 }
