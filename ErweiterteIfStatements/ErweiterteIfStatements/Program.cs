4de8d16a7467c7c0451c2d5339e12712f9c87a0f
Lars Gruber <lars.gruber@outlook.com>
2019-01-25 08:47:16 +0100

    DAA changes

---
+++ ErweiterteIfStatements/ErweiterteIfStatements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ErweiterteIfStatements
+{
+    class Program
+    {
+        /* Bedingung ? erster Ausdruck : zweiter Ausdruck
+         * Bedingung muss entweder wahr oder falsch sein
+         * Beispiel: a ? b : c
+         */
+
+        static void Main(string[] args)
+        {
+            int temperatur = -5;
+            string stateOfMatter;
+
+            if (temperatur < 0)
+            {
+                stateOfMatter = "fest";
+            }
+            else
+            {
+                stateOfMatter = "flüssig";
+            }
+
+            Console.WriteLine("Aggregatszustand ist "+stateOfMatter);
+
+            Console.ReadKey();
+        }
+    }
+}
