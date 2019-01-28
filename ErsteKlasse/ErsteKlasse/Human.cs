ed242c59c9d2271fb66e431ae857d5831d844e3b
Lars Gruber <lars.gruber@outlook.com>
2019-01-28 09:15:19 +0100

    DAA changes

---
+++ ErsteKlasse/ErsteKlasse/Human.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ErsteKlasse
+{
+    class Human
+    {
+        //Membervariable
+        string firstname;
+        string lastname;
+        string eyeColor;
+        int age;
+
+        public Human(string firstname, string lastname, string eyeColor, int age)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.eyeColor = eyeColor;
+            this.age = age;
+
+        }
+
+
+
+
+        public void IntroduceMyself()
+        {
+            Console.WriteLine("Hi, Ich bin "+firstname+" "+lastname);
+            Console.WriteLine("Meine Augenfarbe ist "+eyeColor+" und mein Alter "+age);
+
+        }
+
+
+
+    }
+}
---
+++ ErsteKlasse/ErsteKlasse/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ErsteKlasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+
+            Human lars = new Human("Lars", "Gruber","Braun",31);           
+            lars.IntroduceMyself();
+
+            Human franz = new Human("Franz", "Heide","Schwarz",59);            
+            franz.IntroduceMyself();
+        
+     
+
+            Console.ReadKey();
+                    
+
+
+
+        }
+    }
+}
