03ddeb54de22fa61600691086f060186a974d775
Lars Gruber <lars.gruber@outlook.com>
2019-02-08 10:20:54 +0100

    DAA

---
+++ Properties/Properties/Box.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Properties
+{
+    class Box
+    {
+        //membervariablen
+        private string color = "weiß";
+        int length;
+        int width;
+
+        int volume;
+
+        public Box (int length, int height, int width)
+        {
+            this.length = length;
+            this.Height = height;
+            this.width = width;
+            
+        }
+
+        //auto-implemented property / prop 2x TAB
+        public int Height { get; set; }
+
+        public int Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                if (value < 0) //throw new Exception("Breite sollte positiv sein!");
+                {
+                    value = -value;
+                }
+                width = value;
+                
+            }
+        }
+
+        public int Length
+        {
+            get => length;
+            set => length = value;
+        }
+
+        /*
+        public int Length
+        {
+            get
+            {
+                return length;
+
+            }
+            set
+            {
+                length = value;
+            }
+        }*/
+
+
+
+        public int FrontSurface
+        {
+            get
+            {
+                return Height * length;
+            }
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Länge ist {0} und Höhe ist {1} und Breite ist {2}, somit ist das Volumen {3}", length, Height, width, volume = length*Height
+                *width);
+        }
+    }
+}
---
+++ Properties/Properties/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box(5,3,4);
+            Console.WriteLine("Die Front-Fläche ist {0}", box.FrontSurface);
+          
+
+            box.DisplayInfo();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
